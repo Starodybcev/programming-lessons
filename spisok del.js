23b6e326ade9a3568d1a7fe131dd04a14eb97d06
Starodybcev <sholoh.sholohov@mail.ru>
2022-03-31 12:07:03 +0300

    читает картинки с сайта

--- spisok del.js
+++ spisok del.js
@@ -1,14 +1,10 @@
 const axios = require('axios');
-// Make a request for a user with a given ID
 axios.get('https://http.cat/[status_code]')
 .then(function (response) {
-// handle success
 console.log(response);
 })
 .catch(function (error) {
-// handle error
 console.log(error);
 })
 .then(function () {
-// always executed
 });
