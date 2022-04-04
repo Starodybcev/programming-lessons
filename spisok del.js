97cd105047e171039100c5da9fc897cc8d919dd5
Starodybcev <sholoh.sholohov@mail.ru>
2022-04-04 10:00:55 +0300

    скачиваю картинки в файл

--- spisok del.js
+++ spisok del.js
@@ -1,10 +1,15 @@
 const axios = require('axios');
 axios.get('https://http.cat/[status_code]')
 .then(function (response) {
-console.log(response);
+console.log(response.data);response.data 
 })
 .catch(function (error) {
 console.log(error);
 })
 .then(function () {
 });
+
+
+fs=require("fs")
+fs.writeFileSync("txt.txt", response.data,  "ascii")
+  
