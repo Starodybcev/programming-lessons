6d96ed6dc9028b8ad4703baef7b67ab7d57be25f
Starodybcev <sholoh.sholohov@mail.ru>
2022-10-12 12:37:07 +0300

    html css стили

--- index.js
+++ index.js
@@ -3,16 +3,17 @@ const express = require("express");
 const app = express();
 
 app.use(express.urlencoded({
-extended: false
+    extended: false
 }));
 
 app.get("/", function (request, response) {
-response.sendFile(__dirname + "/form.html");
+    response.sendFile(__dirname + "/.html");
 });
-app.post("/",function (request, response) {
-if (!request.body) return response.sendStatus(400);
-console.log(request.body);
-response.send(`${request.body.login} - ${request.body.password} - ${request.body.gender}-${request.body.firstname} - `);
+app.post("/", function (request, response) {
+    if (!request.body) return response.sendStatus(400);
+    console.log(request.body);
+   
+
 });
 
 app.listen(3000, () => console.log("Сервер запущен..."));
